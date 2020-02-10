eca768310d8674d9806a74118fa4e481caf4b9a0
Michelle Johnson <bob1234girl@gmail.com>
2020-02-10 14:10:36 -0600

    completed ch4

--- libraryproject/libraryapp/urls.py
+++ libraryproject/libraryapp/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.urls import include, path
 from .views import *
 
 app_name = "libraryapp"
 
 urlpatterns = [
-    path('', book_list, name='home'),
+    path('', home, name='home'),
     path('books/', book_list, name='books'),
     path('librarians/', librarian_list, name='librarians'),
-
+    path('libraries/', library_list, name='libraries'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', logout_user, name='logout'),
 ]   
