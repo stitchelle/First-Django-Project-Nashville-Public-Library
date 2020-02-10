43b2160695850f7df5ad774d6e2d6c97d2a84fcb
Michelle Johnson <bob1234girl@gmail.com>
2020-02-10 11:39:01 -0600

    got book list to display on browser

--- libraryproject/libraryapp/models/book.py
+++ libraryproject/libraryapp/models/book.py
@@ -10,7 +10,7 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     isbn = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
-    yearpublished = models.IntegerField()
+    year_published = models.IntegerField()
     location = models.ForeignKey(Library, on_delete=models.CASCADE)
     librarian = models.ForeignKey(Librarian, on_delete=models.CASCADE)
 
